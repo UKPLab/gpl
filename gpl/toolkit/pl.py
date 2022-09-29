06f1821b8e944cd4bfdba90025fb56d5bdaf4360
kwang2049 <kexin.wang.2049@gmail.com>
2022-09-29 14:57:06 +0200

    Batch size larger than data size

--- gpl/toolkit/pl.py
+++ gpl/toolkit/pl.py
@@ -66,6 +66,15 @@ class PseudoLabeler(object):
         return decoded
 
     def run(self):
+        number_of_data_points = len(self.hard_negative_dataloader.dataset)
+        batch_size = self.hard_negative_dataloader.batch_size
+
+        if number_of_data_points < batch_size:
+            raise ValueError(
+                "Batch size larger than number of data points "
+                f"(batch size: {batch_size}, number of data points: {number_of_data_points})"
+            )
+
         # header: 'query_id', 'positive_id', 'negative_id', 'pseudo_label_margin'
         data = []
 
